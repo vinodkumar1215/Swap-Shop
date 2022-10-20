734dce439b6d6b0a64b1d10cc48cf29a324bf415
rajyalakshmi20 <rajyalakshmidhullipalla20@gmail.com>
2022-10-20 10:50:13 +0530

    added product list

---
+++ src/components/WishList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BsHeart } from "react-icons/bs";
+// import "./WishList.css";
+
+export default function WishList() {
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <BsHeart />
+      </div>
+    </div>
+  );
+}
